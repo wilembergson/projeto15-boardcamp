b8f9c40e4cd9b71771b8d26064ff3c68374a6df4
wilembergson <wilembergson@yahoo.com>
2022-05-28 14:59:27 -0300

    Routes games implemented

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -16,12 +16,21 @@ export async function addGame(req, res){
 
 //LISTAR GAMES
 export async function listGames(req, res){
+    const {name} = req.query
+    let sql = ''
+    if(name){
+        const nameCopy = name.charAt(0).toUpperCase() + name.slice(1);
+        sql = `SELECT games.*, categories.name as "categoryName"
+        FROM games
+        JOIN categories ON games."categoryId"=categories.id
+        WHERE games.name LIKE '${nameCopy}%';`
+    }else{
+        sql = `SELECT games.*, categories.name as "categoryName"
+        FROM games
+        JOIN categories ON games."categoryId"=categories.id;`
+    }
     try{
-        const result = await db.query(
-            `SELECT games.*, categories.name as "categoryName"
-             FROM games
-             JOIN categories
-             ON games."categoryId"=categories.id;`)
+        const result = await db.query(sql)
         return res.status(200).send(result.rows)
     }catch(e){
         console.log(e)
